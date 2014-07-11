3db4863638a0d34fcaaae179d209ac5d0af493fe
2Wrongs <lhenkel@gmail.com>
2014-07-11 17:46:13 -0600

    Renamed packages from example.com to henkelsoft.com
    Added a prefs page, but didn't wire it up.
    
    This is where I fouled everything up last time, but at least it doesn't crash here..

---
+++ src/com/henkelsoft/trekyourself/MonitoredServer.java
@@ -0,0 +1,39 @@
+package com.henkelsoft.trekyourself;
+
+public class MonitoredServer {
+
+	private String serverName;
+	private String serverStatus;
+	private String serverMessage;
+	
+	MonitoredServer(String serverNameIn) {
+		serverName = serverNameIn;
+	}
+
+	MonitoredServer() {
+		
+	}
+	
+	
+	public void setServerStatus(String statusIn) {
+		serverStatus = statusIn;
+	}
+	
+	public void setServerMessage(String messageIn) {
+		serverMessage = messageIn;
+	}
+
+	public void setServerName(String nameIn) {
+		serverName = nameIn;
+	}	
+	
+	public String getName() {
+		return serverName;
+	}
+	public String getStatus() {
+		return serverStatus;
+	}
+	public String getMessage() {
+		return serverMessage;
+	}
+}
